562311d6898561e4c5115a2a876dab9f8f23bb0c
petritmekuli <petritmekuli2013@gmail.com>
2022-11-30 17:10:44 +0100

    change extends to the Facade class provided from laravel(StageThree)

--- app/Tutorials/Facades/StringModifierFacade.php
+++ app/Tutorials/Facades/StringModifierFacade.php
@@ -1,6 +1,15 @@
 <?php
 namespace App\Tutorials\Facades;
 
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * Facades functionality is similar to dependency injection and helpers.
+ * With helpers I believe is identical because they are like wrappers and
+ * deep down both use the same classes. However when facade is compared
+ * with dependency injection, I could say that dependency injection has
+ * the ability to swap implementations of the injected classes.
+ */
 class StringModifierFacade extends Facade{
     public static function getFacadeAccessor(){
         return 'stringModifier';
