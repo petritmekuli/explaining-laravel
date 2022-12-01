edbf650a419474511608e2c4c14aaabe82b6c014
petritmekuli <petritmekuli2013@gmail.com>
2022-12-01 14:22:34 +0100

    Simpliest validation way using validate() in the  instance

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,6 +16,31 @@ class PostController extends Controller
     }
 
     public function store(Request $request){
+        /**
+         * The simplest form to validate a from request is using a validate() function
+         * that is available to the $request object. This function not only that makes sure
+         * the form data meet the constrains set by rules. Also makes sure that the error
+         * messages and old input form data are going to be flashed in the session. As well
+         * redirect back to the same view where the form has been submitted. Error messages
+         * are shared to the views through a middleware called ShareErrorsFromSession
+         * which pulls the errors form session if there are any, and shares the $errors
+         * attribute to all the views. That's how $errors inside the view files is set.
+         * $errors is an instance of MessageBag, so there are some helpful methods available.
+         * $errors is available in every view so even if there are no errors it will have null
+         * value but no extra isset checks needed. Similar functionality could have been applied
+         * through ServiceProviders using Facades\View::share(). Beside validation rules that's
+         * all about validation using validate() on the request. Editing messages using this way
+         * can be changed only in the lang/xx/validation.php. This is because most of the times
+         * you don't need to make any changes. But if there are times then you could create a custom
+         * validation instance using Validator::make();
+         */
+
+        // Validating the request input
+        $request->validate([
+            'title' => ['required','alpha', 'min:3'],
+            'body' => ['required', 'alpha']
+        ]);
+
         Post::create($request->only(['title', 'body']));
         return redirect()->route('post.index');
     }
--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -11,12 +11,23 @@
         <input type="hidden" name="_token" value="{{ csrf_token() }}" />
 
         <label for="title">Title</label>
-        <input type="text" name="title">
+        <input type="text" name="title" value="{{old('title')}}">
+        @error('title')
+            @foreach ($errors->get('title') as $titleError)
+                {{--This because there could be more than one title errors--}}
+                <span style="color:red">{{$titleError}}</span>
+            @endforeach
+        @enderror
 
         </br>
 
         <label for="title">Body</label>
-        <input type="text" name="body">
+        <input type="text" name="body" value="{{old('body')}}">
+        @if($errors->has('body'))
+            @foreach ($errors->get('body') as $bodyError)
+                <span style="color:red">{{$bodyError}}</span>
+            @endforeach
+        @endif
 
         </br>
         {{-- <input type="submit" value="Create"> --}}
