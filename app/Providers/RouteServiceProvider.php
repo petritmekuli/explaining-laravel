972b6ce566111bfb9939f52783b917bee0692e5c
petritmekuli <petritmekuli2013@gmail.com>
2023-01-17 01:18:50 +0100

    Showing an example of throttle middleware and RateLimiter

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -48,5 +48,30 @@ class RouteServiceProvider extends ServiceProvider
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
+        /**
+         * This is an example to stop users from updating profile info more than
+         * 3 times within a minute. This is done using throttle middleware and passing
+         * in this case update_settings Rate limiter.
+         * This is one way to stop too many request but you can't do a lot more here.
+         * Like you could inside the controller actions, interacting directly with the
+         * Rate Limiter. You can show things like attempts left, time to retry etc.
+         * A great example is locking login attempts to five. If you try more than five
+         * within a minute an error message will be shown, showing you that only five
+         * attempts are allowed within a min. Also shows the remaining time.
+         */
+        RateLimiter::for('update_settings', function (Request $request) {
+            return Limit::perMinute(3)->by(auth()->user()->id);
+        });
+        /**
+         * How RateLimiter actually works is by caching two picies of information for a
+         * specific key which should be unique for each user. So for example email address
+         * petrit@example.com(key): number of attempts(value)
+         * petrit@example.com:time(key) : now time in seconds(value)
+         *
+         * And RateLimiter reads and writes to this data and it also shows all the info
+         * using functions like: tooManyRequest, attempts, availableIn, hit, resetAttempts
+         * etc. You could use either of RateLimiter facade or RateLimiter directly.
+         */
     }
+
 }
