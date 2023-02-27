1829758c9cffa932bf7f173381cefe6432a96aa9
petritmekuli <petritmekuli2013@gmail.com>
2023-02-27 20:41:47 +0100

    Add 'User' alias instead 'App\Models\User' for easier use in tinker

---
+++ config/tinker.php
@@ -0,0 +1,8 @@
+<?php
+    return [
+        'aliases' => [
+            'User' => App\Models\User::class,
+            // 'Comment' => App\Models\Comment::class,
+            // add more model aliases here
+        ],
+    ];
